6f08b0118c534e6dc518ce740d22df41c2f64a60
mchoudhry2000 <73971576+mchoudhry2000@users.noreply.github.com>
2020-11-05 14:30:33 -0800

    Update Hello.java

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("SacStateID: 302032146 - This line was added by Valentin Bakalor.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID: 218714406 - This line was added by Momin Choudhry.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
