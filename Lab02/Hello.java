289fff73a2cbd5fa50cba95269d41c1f2b935a6b
Jeff Anjelo <Jeff Anjelo@DESKTOP-THRNE7J>
2020-11-05 15:04:23 -0800

    Added my first and last name

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("SacStateID: 302032146 - This line was added by Valentin Bakalor.");
 		System.out.println("SacStateID: 218714406 - This line was added by Momin Choudhry.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID: 220036558 - This line was added by Jeff Anjelo Rondilla.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
