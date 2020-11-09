344227731153a7dd3c31170af0597b3bee7b7006
leoba <leoba@Razer-Leo>
2020-11-09 15:41:37 -0800

    test1

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -16,7 +16,7 @@ public class Hello {
 		System.out.println("SacStateID: 301765464 - This line was added by Jacob Myers.");
 		System.out.println("SacStateID: 220036558 - This line was added by Jeff Anjelo Rondilla.");
 >>>>>>> branch 'master' of https://github.com/SlavStack/Lab02.git
-		System.out.println("SacStateID - This line was added by First name Last name.");
+
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
